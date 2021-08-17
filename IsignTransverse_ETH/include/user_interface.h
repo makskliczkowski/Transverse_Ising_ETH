35b7292d465be4c226fc13c1f5d974b645fd82e9
Maksymilian Kliczkowski <maxgrom97@gmail.com>
2021-08-17 21:46:40 +0200

    pluplu

--- IsignTransverse_ETH/include/user_interface.h
+++ IsignTransverse_ETH/include/user_interface.h
@@ -3,6 +3,7 @@
 #define UI
 #include "headers.h"
 
+
 std::vector<std::string> change_input_to_vec_of_str(int argc, char** argv);
 
 class user_interface{
@@ -10,7 +11,17 @@ protected:
 	int thread_number;																						// number of threads
 	int boundary_conditions;																				// boundary conditions - 0 - PBC, 1 - OBC, 2 - ABC,... 
 
-	std::string saving_dir;
+	std::string saving_dir;																					// directory for files to be saved onto
+	std::string getCmdOption(const v_1d<std::string>& vec, std::string option) const;					 	// get the option from cmd input
+	
+	template <typename T>
+	void set_option(T& value,const v_1d<std::string>& argv, std::string choosen_option, bool geq_0 = true);	// set an option
+
+	template <typename T>
+	void set_default_msg(T& value,std::string option, std::string message,\
+		const std::unordered_map <std::string, std::string>& map) const;									// setting value to default and sending a message							 		
+	// std::unique_ptr<LatticeModel> model;															 			// a unique pointer to the model used
+
 public:
 	virtual void exit_with_help() = 0;
 /* REAL PARSING */
@@ -27,52 +38,40 @@ public:
 namespace isingUI
 {
 	/* MAP FOR STRINGS */
-	enum class parsers {
-		f,						// file to read from directory
-		J,						// spin coupling
-		h,						// perpendicular magnetic field constant
-		g,						// transverse magnetic field constant
-		w,						// disorder strength
-		L,						// chain length
-		b,						// boundary condition
-		m,						// choose model
-		p,						// use parity symmetry?
-		th,						// number of threads
-		q						// quit with help
-	};
 	/// <summary>
 	/// The map is used to parse also the two letters
 	/// cases and create string variable from enum
 	/// </summary>
-	std::unordered_map <std::string, parsers> const table{
-		{"f",parsers::f},
-		{"J",parsers::J},
-		{"h",parsers::h},
-		{"g",parsers::g},
-		{"w",parsers::w},
-		{"L",parsers::L},
-		{"b",parsers::b},
-		{"m",parsers::m},
-		{"p",parsers::p},
-		{"th",parsers::th},
-		{"q",parsers::q}
+	std::unordered_map <std::string, std::string> const table{
+		{"f",""},								// file to read from directory
+		{"J","1.0"},					// spin coupling
+		{"h","0.0"},					// perpendicular magnetic field constant
+		{"g","0.0"},					// transverse magnetic field constant
+		{"w","0.0"},					// disorder strength
+		{"L","4"},						// chain length
+		{"b","0"},						// boundary condition
+		{"m","0"},						// choose model
+		{"p","0"},						// use parity symmetry?
+		{"th","1"},						// number of threads
+		{"q","0"}						// quit with help
 	};
 
 	class ui: public user_interface{
 	protected:
-		/* MODEL PARAMETERS */
+	// MODEL PARAMETERS
 		std::vector<double> J;
 		double h,g,w;
 		int L,b,m;
-		bool p;
+		bool p,q;
 	public:
-	/* CONSTRUCTORS */
+	// CONSTRUCTORS 
 		ui() = default;
-	/* PARSER FUNCTION FOR HELP */
+		ui(int argc, char** argv);																	// standard constructor
+	// PARSER FUNCTION FOR HELP 
 		void exit_with_help() override;
-	/* REAL PARSER */
+	// REAL PARSER
 		void parseModel(int argc, std::vector<std::string> argv) override;							// the function to parse the command line
-	/* HELPING FUNCIONS */
+	// HELPING FUNCIONS 
 		void set_default() override;																// set default parameters
 		void set_J(int mode, std::string argument);													// setting J according to the given mode
 		void correct_J_size(int mode);																// if L is inconsistent with J size, we must add or erease elements according to L
